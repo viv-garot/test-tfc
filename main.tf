ec490e69ef893bd4ce808914a2ac42256d51b173
viv-garot <vivien@hashicorp.com>
2021-07-26 14:42:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,6 @@ output "caller_user" {
 resource "aws_instance" "ubuntu" {
   ami           = "ami-0baa12826217a765e"
   instance_type = "t2.macro"
-  vpc_security_group_ids      = "vpc-3302f159"
+  vpc_security_group_ids      = "sg-01b2d192db100cf5e"
   subnet_id                   = "subnet-2b62b941"
 }
